60ef2519f7730d27fd15bd63cc1bd92bc943a728
GustavoFSProg <gustavo.prog40@gmail.com>
2024-10-04 09:46:32 -0300

    rotas

--- apps/backend/src/produto/produto.controller.ts
+++ apps/backend/src/produto/produto.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Produto } from '../../../../packages/core/dist';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { PrismaProduto } from './prisma.produto';
 
 @Controller('produtos')
-export class ProdutoController {}
+export class ProdutoController {
+    constructor(private readonly repo: PrismaProduto) {}
+
+    @Post()
+    salvarProduto(@Body() produto: Produto): Promise<void> {
+      return this.repo.salvar(produto);
+    }
+  
+    @Get()
+    obterProdutos(): Promise<Produto[]> {
+      return this.repo.obter();
+    }
+  
+    @Get(':id')
+    obterProduto(@Param('id') id: string): Promise<Produto> {
+      return this.repo.obterPorId(+id);
+    }
+  
+    @Delete(':id')
+    excluirProduto(@Param('id') id: string): Promise<void> {
+      return this.repo.excluir(+id);
+    }
+
+}
--- apps/backend/src/produto/produto.module.ts
+++ apps/backend/src/produto/produto.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProdutoController } from './produto.controller';
+import { DbModule } from 'src/db/db.module';
+import { PrismaProduto } from './prisma.produto';
 
 @Module({
-  controllers: [ProdutoController]
+  imports: [DbModule],
+  controllers: [ProdutoController],
+  providers: [PrismaProduto]
+
 })
 export class ProdutoModule {}
